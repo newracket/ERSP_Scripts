e5820703bb068d167a693ae7ddaede016d637b7f
newracket <newracket@users.noreply.github.com>
2024-02-04 20:10:51 -0800

    Add support for reading formulas and cross-sheet references in Excel files

--- script.py
+++ script.py
@@ -8,6 +8,7 @@ from openpyxl import load_workbook
 import win32com.client as win32
 
 import os
+import re
 
 
 def convert_xls_to_xlsx(file_name):
@@ -31,13 +32,53 @@ def convert_all_spreadsheets_to_xlsx(dir_path):
           print('Error converting file: ' + file_path)
 
 def read_file(file_name):
-  convert_xls_to_xlsx(file_name)
-  wb = load_workbook(file_name + 'x')
-  ws = wb['Sheet2']
+  # convert_xls_to_xlsx(file_name)
+  wb = load_workbook(file_name)
+  print(file_name + '\n')
 
-  print(ws['A1'].value)
+  for sheet in wb.sheetnames:
+    ws = wb[sheet]
 
-convert_all_spreadsheets_to_xlsx('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets')
+    if '<Worksheet' not in str(ws):
+      continue
+
+    # Starts with = means formula
+    # regex would be like [A-Z]{0-9}* : [A-Z]{0-9}* 
+    # 
+
+
+    for row in ws.iter_rows():
+      for cell in row:
+        cell_value = cell.value
+        cross_sheet_pattern = r'^=[a-zA-Z \d]*![A-Z]\d*'
+        formula_pattern = r'^=[A-Z]+\([A-Z]+\d+:[A-Z]+\d+\)'
+        if not isinstance(cell_value, str):
+          continue
+
+        if re.match(cross_sheet_pattern, cell_value):
+          print(f"Cross-Sheet-Reference; Sheet: {sheet}, Cell {cell.coordinate}: {cell_value}")
+        elif re.match(formula_pattern, cell_value):
+          print(f"Formula; Sheet: {sheet}, Cell {cell.coordinate}: {cell_value}")
+
+  print('\n')
+
+def read_files_in_directory(dir_path):
+  for root, dirs, files in os.walk(dir_path):
+    for file in files:
+      file_path = os.path.join(root, file)
+
+      if (file_path.endswith('.xlsx')):
+        read_file(file_path)
+
+  # ws = wb['budget']
+  # print(ws['J46'].value)
+
+
+  
+
+# convert_all_spreadsheets_to_xlsx('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets')
 # convert_xls_to_xlsx('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\cross_sheet.xls')
 # convert_xls_to_xlsx('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\no_cross_sheet.xls')
 # read_file('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\cross_sheet.xls')
+# read_file('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets\\database\\original\\0XLSBudgetingWGP2.xlsx')
+read_files_in_directory('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets\\database')
