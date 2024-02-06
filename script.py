2dab6d4d00870cb1a66af47b3214a6b31fb13a1a
newracket <newracket@users.noreply.github.com>
2024-02-06 15:15:48 -0800

    ran script

--- script.py
+++ script.py
@@ -31,10 +31,16 @@ def convert_all_spreadsheets_to_xlsx(dir_path):
         except:
           print('Error converting file: ' + file_path)
 
+crossSheetNum = 0
+rangeRefNum = 0
+noneNum = 0
+totalNum = 0
+
 def read_file(file_name):
-  # convert_xls_to_xlsx(file_name)
+  global crossSheetNum, rangeRefNum, noneNum, totalNum
+
   wb = load_workbook(file_name)
-  print(file_name + '\n')
+  relative_path = file_name.split('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets\\')[1]
 
   for sheet in wb.sheetnames:
     ws = wb[sheet]
@@ -42,36 +48,48 @@ def read_file(file_name):
     if '<Worksheet' not in str(ws):
       continue
 
-    # Starts with = means formula
-    # regex would be like [A-Z]{0-9}* : [A-Z]{0-9}* 
-    # 
-
-
+    localCrossSheetNum = 0
+    localRangeRefNum = 0
     for row in ws.iter_rows():
       for cell in row:
         cell_value = cell.value
         cross_sheet_pattern = r'^=[a-zA-Z \d]*![A-Z]\d*'
-        formula_pattern = r'^=[A-Z]+\([A-Z]+\d+:[A-Z]+\d+\)'
+        range_reference_pattern = r'^=[A-Z]+\([A-Z]+\d+:[A-Z]+\d+\)'
         if not isinstance(cell_value, str):
           continue
 
         if re.match(cross_sheet_pattern, cell_value):
-          print(f"Cross-Sheet-Reference; Sheet: {sheet}, Cell {cell.coordinate}: {cell_value}")
-        elif re.match(formula_pattern, cell_value):
-          print(f"Formula; Sheet: {sheet}, Cell {cell.coordinate}: {cell_value}")
+          localCrossSheetNum += 1
+          with open('cross-sheet.txt', 'a') as f:
+            f.write(f"{relative_path} Sheet: {sheet}, Cell {cell.coordinate}: {cell_value}\n")
+        elif re.match(range_reference_pattern, cell_value):
+          localRangeRefNum += 1
+          with open('range-ref.txt', 'a') as f:
+            f.write(f"{relative_path} Sheet: {sheet}, Cell {cell.coordinate}: {cell_value}\n")
 
-  print('\n')
+  if localCrossSheetNum > 0:
+    crossSheetNum += 1
+  if localRangeRefNum > 0:
+    rangeRefNum += 1
+  if localCrossSheetNum == 0 and localRangeRefNum == 0:
+    noneNum += 1
+    with open('none.txt', 'a') as f:
+      f.write(f"{relative_path}\n")
+  totalNum += 1
 
 def read_files_in_directory(dir_path):
+  global crossSheetNum, rangeRefNum, noneNum, totalNum
   for root, dirs, files in os.walk(dir_path):
     for file in files:
       file_path = os.path.join(root, file)
 
       if (file_path.endswith('.xlsx')):
-        read_file(file_path)
-
-  # ws = wb['budget']
-  # print(ws['J46'].value)
+        try:
+          read_file(file_path)
+          print(f'Cross Sheet: {crossSheetNum}, Range Reference: {rangeRefNum}, None: {noneNum}, Total: {totalNum}')
+        except:
+          with open('error.txt', 'a') as f:
+            f.write(f'Error reading file: {file_path}\n')
 
 
   
@@ -81,4 +99,4 @@ def read_files_in_directory(dir_path):
 # convert_xls_to_xlsx('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\no_cross_sheet.xls')
 # read_file('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\cross_sheet.xls')
 # read_file('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets\\database\\original\\0XLSBudgetingWGP2.xlsx')
-read_files_in_directory('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets\\database')
+read_files_in_directory('C:\\Users\\anike\\OneDrive\\Documents\\UCSD\\ERSP\\Script\\spreadsheets')
